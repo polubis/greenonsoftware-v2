a8e9e3562c0c3f6ad52c17f2da367312becff86b
polubis <polubis@wp.pl>
2025-08-18 09:13:43 +0200

    feat(4focus): add tests for internal and external contracts creation

--- apps/4focus/src/lib/clean-api/__tests__/params-construct.test.ts
+++ apps/4focus/src/lib/clean-api/__tests__/params-construct.test.ts
@@ -1,6 +1,6 @@
 import { afterEach, beforeEach, describe, expectTypeOf, it, vi } from "vitest";
-import type { ErrorVariant } from "../index";
-import { contract } from "../index";
+import type { ErrorVariant } from "..";
+import { cleanAPI, contract } from "..";
 
 vi.mock("axios");
 
@@ -53,6 +53,37 @@ describe("params construction works when", () => {
     };
 
     const specificContract = contract<Contracts>()({
+      "path-params": {
+        method: "get",
+        path: "/something/:id",
+      },
+      "bad-path-params-with-extra-prop": {
+        // @ts-expect-error - extra prop
+        method: "get",
+        // @ts-expect-error - extra prop
+        path: "/something/:id/:extra",
+      },
+      "bad-path-params-with-missing-prop": {
+        // @ts-expect-error - missing prop
+        method: "get",
+        // @ts-expect-error - missing prop
+        path: "/something/:",
+      },
+      "bad-path-params-with-missing-slash": {
+        // @ts-expect-error - missing slash
+        method: "get",
+        // @ts-expect-error - missing slash
+        path: "something/:id",
+      },
+      "bad-path-params-with-typo-in-param-name": {
+        // @ts-expect-error - typo in param name
+        method: "get",
+        // @ts-expect-error - typo in param name
+        path: "/something/:id/:extra",
+      },
+    });
+
+    cleanAPI<Contracts>()({
       "path-params": {
         method: "get",
         path: "/something/:id",
